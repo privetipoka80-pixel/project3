3f14dd1d2cfb8f4d469541644d65955b65e6d3e2
privetipoka80-pixel <privetipoka80@gmail.com>
2026-04-12 16:07:35 +0300

    Create users

---
+++ data/users.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+from data.db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    username = sa.Column(sa.String, nullable=True, unique=True)
+    email = sa.Column(sa.String, nullable=True, unique=True)
+    hashed_password = sa.Column(sa.String, nullable=True)
+    role = sa.Column(sa.String, nullable=True, default='user')
+    created_at = sa.Column(sa.DateTime, default=sa.func.now())
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def is_seller(self):
+        return self.role == 'seller'
