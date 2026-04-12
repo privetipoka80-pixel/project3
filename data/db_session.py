eb9cde432b5ef9833ebd286e3f2e4ab2e0847cbd
privetipoka80-pixel <privetipoka80@gmail.com>
2026-04-12 16:06:26 +0300

    Create db_session

---
+++ data/db_session.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+
+SqlAlchemyBase = orm.declarative_base()
+
+__factory = None
+
+def global_init(db_file):
+    global __factory
+    if __factory:
+        return
+
+    conn_str = f'sqlite:///{db_file}?check_same_thread=False'
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+
+    from data import users, products
+    SqlAlchemyBase.metadata.create_all(engine)
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
