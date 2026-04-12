be14090f304586642a296c7d944dcadb19bdcba4
privetipoka80-pixel <privetipoka80@gmail.com>
2026-04-12 16:09:30 +0300

    Initialize database connection in main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import data.db_session as db_session
+
+db_session.global_init('db/shop.db')
 
 app = Flask(__name__)
 app.secret_key = 'dev_secret_key_123'
