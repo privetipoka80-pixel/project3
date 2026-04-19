4d89550972c12019d0dd9926fa2f195f41211495
delta09-yan <denderen09@gmail.com>
2026-04-19 12:26:19 +0300

    Update products.py
    added seller and images

--- data/products.py
+++ data/products.py
@@ -12,3 +12,5 @@ class Product(SqlAlchemyBase):
     stock = sa.Column(sa.Integer, nullable=True, default=0)
     seller_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'))
     created_at = sa.Column(sa.DateTime, default=sa.func.now())
+    seller = sa.orm.relationship("User", back_populates="products")
+    images = sa.orm.relationship("ProductImage", back_populates="product", cascade="all, delete-orphan")
