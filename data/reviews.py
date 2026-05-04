55cf6b7abf7b0c4ee3af1159f76cfb5a86ab7276
privetipoka80-pixel <privetipoka80@gmail.com>
2026-05-04 21:59:35 +0300

    Add Review model for product feedback

--- data/reviews.py
+++ data/reviews.py
@@ -3,6 +3,7 @@ import sqlalchemy
 from data.db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 
+
 class Review(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'reviews'
 
@@ -16,14 +17,15 @@ class Review(SqlAlchemyBase, SerializerMixin):
                                 nullable=False)
     rating = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
     comment = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
-    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    
+    created_at = sqlalchemy.Column(
+        sqlalchemy.DateTime, default=datetime.datetime.now)
+
     product = sqlalchemy.orm.relationship("Product", back_populates="reviews")
     user = sqlalchemy.orm.relationship("User", back_populates="reviews")
-    
+
     def __repr__(self):
         return f'<Review> {self.id} - Product {self.product_id} - Rating {self.rating}'
-    
+
     @property
     def username(self):
         return self.user.username if self.user else "Unknown"
